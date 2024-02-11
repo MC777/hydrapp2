31c154f1ef7e0f4f5b69321441bda7a1bc67a211
Marek <ciesla.marek@gmail.com>
2024-02-11 23:01:56 +0100

    added markup and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
