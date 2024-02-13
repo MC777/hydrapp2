7104ada3a6e5faa676575c985c344b3b7d21007f
Marek <ciesla.marek@gmail.com>
2024-02-13 13:57:13 +0100

    added local storage support

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ const subtractButton = document.querySelector(".subtract--js");
 const counterValue = document.querySelector(".counter--js");
 
 let glasses = 0;
+const key = new Date().toISOString().slice(0, 10);
+
+if (localStorage.getItem(key)) {
+  glasses = parseInt(localStorage.getItem(key));
+}
 
 const setCounterValue = (value) => {
   counterValue.innerHTML = value;
+  localStorage.setItem(key, value);
 };
 
 setCounterValue(glasses);
