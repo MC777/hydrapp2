4a57134e687c3a42a1270aaee818a8d47c11fcc7
Marek <ciesla.marek@gmail.com>
2024-02-13 13:46:11 +0100

    added js logic for add and subtract

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-alert('test')
+const addButton = document.querySelector(".add--js");
+const subtractButton = document.querySelector(".subtract--js");
+const counterValue = document.querySelector(".counter--js");
+
+let glasses = 0;
+
+const setCounterValue = (value) => {
+  counterValue.innerHTML = value;
+};
+
+setCounterValue(glasses);
+
+addButton.addEventListener("click", () => {
+  glasses = glasses + 1;
+  setCounterValue(glasses);
+});
+
+subtractButton.addEventListener("click", () => {
+  if (glasses >= 1) {
+    glasses = glasses - 1;
+    setCounterValue(glasses);
+  }
+});
